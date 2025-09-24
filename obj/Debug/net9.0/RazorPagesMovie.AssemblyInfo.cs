55164281dd54dc3d5fb231dbe37fae85c31361ed
Tuguer <202100296@estudantes.ips.pt>
2025-09-24 01:56:39 +0100

    git ignore

--- obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15030f4f45927aff544f29a4a7cc42cb01866f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
